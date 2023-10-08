682944546f5c5cbbffb2ae90ba9e61b3cae52186
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2023-10-08 05:00:47 +0100

    query database data from php

--- src/test_db.php
+++ src/test_db.php
@@ -8,14 +8,21 @@ $database = getenv("MYSQL_DATABASE");
 $password = "secret";
 $database = "khalid";
 
+
+$conn = "";
+
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo nl2br("Connected successfully\n");
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
+    die;
 }
+
+$stmt = $conn->query("SELECT * FROM tests")->fetchAll();
+print_r($stmt);
 
 #echo getenv("MYSQL_DATABASE");
 
